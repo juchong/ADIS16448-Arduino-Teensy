1309dbfdeacb1c40a3dc9f819a3b4d00dcbfcb1a
Juan Chong <juan@highcurrent.io>
2017-12-09 02:10:34 -0500

    Added a lookup-table crc calculation example

--- crc/crc16adis.m
+++ crc/crc16adis.m
@@ -29,7 +29,7 @@
 %   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%
 function crc = crc16adis(data)
 
 %Example Data (LSB-first) - Result should be 0x9AF4 (39668):
